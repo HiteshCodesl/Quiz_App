2bb79f3e737dff904f29e6fa1285208a0708d279
Hitesh Navare <navarehitesh65@gmail.com>
2026-03-28 20:51:51 +0530

    added

--- src/quiz.ts
+++ src/quiz.ts
@@ -1,15 +1,53 @@
+import { IoManager } from "./managers/IoManager.js";
 
+export interface Problem {
+    id: string;
+    title: string;
+    description: string;
+    image: string;
+    answer: string;
+    options: {
+        id: number;
+        title: string;
+    }
+}
 
-export class Quiz{
+export class Quiz {
     private hasStarted: boolean;
     private roomId: string;
+    private problems: Problem[];
+    private activeProblem: number;
 
-    constructor(roomId: string){
-      this.hasStarted = false;
-      this.roomId = roomId
+    constructor(roomId: string) {
+        this.hasStarted = false;
+        this.roomId = roomId;
+        this.problems = [];
+        this.activeProblem = 0;
     }
 
-    startQuiz(roomId: string){
+    startQuiz(roomId: string) {
         this.hasStarted = true;
+        const io = IoManager.getInstance();
+
+        io.emit("Change_PROBLEM", {
+            problem: this.problems[0]
+        })
+    }
+
+    next(roomId: string) {
+        this.activeProblem++;
+        const problem = this.problems[this.activeProblem];
+
+        const io = IoManager.getInstance();
+
+        if (problem) {
+            io.emit("Next_Question", {
+                problem: problem
+            })
+        } else{
+            io.emit("Quiz_Ended", {
+                problem: problem
+            })
+        }
     }
 }
