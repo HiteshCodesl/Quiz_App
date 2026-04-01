784659bd1d29100735281e0a9d17b6be05168d02
Hitesh Navare <navarehitesh65@gmail.com>
2026-04-01 13:07:17 +0530

    changed the socket responses

--- src/managers/UserManager.ts
+++ src/managers/UserManager.ts
@@ -1,5 +1,6 @@
 import type { Socket } from "socket.io";
 import { QuizManager } from "./QuizManager.js";
+import { Quiz } from "../quiz.js";
 
 const AdminPassword = "ADMIN_PASSWORD";
 
@@ -9,66 +10,70 @@ export type Users = {
 }
 
 export class UserManager {
-    private users: Users[];
     private quizManager;
+
     constructor() {
-        this.users = [];
         this.quizManager = new QuizManager();
     }
 
-
-    addUser(roomId: string, socket: Socket){
-        this.users.push({
-            roomId,
-            socket
-        })
-        this.createHandler(roomId, socket);
+    addUser(socket: Socket) {
+        console.log("Inside addUser");
+        this.createHandler(socket);
     }
 
-    private createHandler(roomId: string, socket: Socket){
-        socket.on("joinroom", (data) => {
-            const userId = this.quizManager.addUser(roomId, data.name);
-            
-            socket.emit("init", {
-                userId,
-                state: this.quizManager.getCurrentState(roomId)
-            })
-        })
+    private createHandler(socket: Socket) {
+        socket.on("message", (raw) => {
+            const parsed = JSON.parse(raw);
+            console.log("parsed")
+            const data = parsed.data;
+            console.log("data", data);
 
-        socket.on("joinroom_admin", (data) => {
-            if(data.password !== AdminPassword){
-                console.error("Password Not Matches")
-                return;
-            }
-            const userId = this.quizManager.addUser(roomId, data.name);
-            
-            socket.emit("admin_init", {
-                userId,
-                state: this.quizManager.getCurrentState(roomId)
-            })
+            if (parsed.type === "joinRoom") {
+                console.log("Inside JoinRoom")
+                const userId = this.quizManager.addUser(data.roomId, data.name);
 
-            socket.on("createProblem", data =>  {
-                this.quizManager.addProblem(data.roomId, data.problem);
-            })
-
-            socket.on("next", data => {
-                this.quizManager.next(data.problem);
-            })
-        })
-
-        socket.on("submit", (data) => {
-            const userId = data.userId;
-            const roomId = data.roomId;
-            const submission = data.submission;
-            const problemId = data.problemId;
-
-            if(submission != 0 || submission != 1 || submission != 2 || submission != 3){
-                console.error("Error WHile Getting Input" + submission);
-                return;
+                socket.emit("init", {
+                    userId,
+                    state: this.quizManager.getCurrentState(data.roomId)
+                })
             }
 
-            this.quizManager.submit(userId, roomId, problemId, submission)
+            if (parsed.type === "joinRoomAdmin") {
+                if (data.password !== AdminPassword) {
+                    console.error("Password Not Matches")
+                    return;
+                }
+
+                if(parsed.subtype === "createQuiz"){
+                    this.quizManager.addQuiz(data.roomId)
+                    console.log("quiz created");
+                }
+
+                if(parsed.subtype ===  "createProblem"){
+                    const {problem, roomId} = parsed.data;
+
+                    this.quizManager.addProblem(roomId, problem);
+                }
+
+                if(parsed.subtype === "next"){
+                    this.quizManager.next(data.problem);
+                }
+            }
+
+            if (parsed.type === "submit") {
+
+                const userId = data.userId;
+                const roomId = data.roomId;
+                const submission = data.submission;
+                const problemId = data.problemId;
+
+                if (submission !== 0 || submission !== 1 || submission !== 2 || submission !== 3) {
+                    console.error("Error WHile Getting Input" + submission);
+                    return;
+                }
+
+                this.quizManager.submit(userId, roomId, problemId, submission)
+            }
         })
     }
-
 }
